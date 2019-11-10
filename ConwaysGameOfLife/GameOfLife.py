2a18fc8a74d5186553fc7a833d6adbc43dc3e318
Splinter1984 <roma13rajon@gmail.com>
2019-11-10 20:53:46 +0800

    it's alive

--- ConwaysGameOfLife/GameOfLife.py
+++ ConwaysGameOfLife/GameOfLife.py
@@ -10,11 +10,12 @@ CELL_SIZE = 5
 DEAD_COLOR = (23, 32, 42)
 ALIVE_COLOR = (93, 173, 226)
 STAT_COLOR = (213, 93, 226)
-SPEED = 0.03
+SPEED = 0
 
 class GameOfLife:
 
     def __init__(self):
+
         pygame.init()
         self.screen = pygame.display.set_mode(BOARD)
         pygame.display.set_caption('ConwaysGameOfLife')
@@ -47,14 +48,55 @@ class GameOfLife:
         pygame.display.flip()
 
     def initial_grid(self):
-        for i in range(self.row):
-            for j in range(self.col):
-                self.active_field[i][j] = random.choice([0, 1])
-        # self.active_field[20][20] = 1
+        # for i in range(self.row):
+        #     for j in range(self.col):
+        #         self.active_field[i][j] = random.choice([0, 1])
+        #----------------------
+        # self.active_field[19][20] = 1
         # self.active_field[20][21] = 1
         # self.active_field[20][22] = 1
-        # self.active_field[19][21] = 1
+        # self.active_field[19][22] = 1
         # self.active_field[18][22] = 1
+        #----------------------
+        self.active_field[1][5] = 1
+        self.active_field[1][6] = 1
+        self.active_field[2][5] = 1
+        self.active_field[2][6] = 1
+
+        self.active_field[36][3] = 1
+        self.active_field[35][3] = 1
+        self.active_field[36][4] = 1
+        self.active_field[35][4] = 1
+        
+        self.active_field[11][5] = 1
+        self.active_field[11][6] = 1
+        self.active_field[11][7] = 1
+        self.active_field[12][4] = 1
+        self.active_field[12][8] = 1
+        self.active_field[13][3] = 1
+        self.active_field[13][9] = 1
+        self.active_field[14][3] = 1
+        self.active_field[14][9] = 1
+        self.active_field[15][6] = 1
+        self.active_field[16][4] = 1
+        self.active_field[16][8] = 1
+        self.active_field[17][5] = 1
+        self.active_field[17][6] = 1
+        self.active_field[17][7] = 1
+        self.active_field[18][6] = 1
+
+        self.active_field[21][3] = 1
+        self.active_field[21][4] = 1
+        self.active_field[21][5] = 1
+        self.active_field[22][3] = 1
+        self.active_field[22][4] = 1
+        self.active_field[22][5] = 1
+        self.active_field[23][2] = 1
+        self.active_field[23][6] = 1
+        self.active_field[25][1] = 1
+        self.active_field[25][2] = 1
+        self.active_field[25][6] = 1
+        self.active_field[25][7] = 1
 
 
     def update_grid(self):
@@ -93,6 +135,7 @@ class GameOfLife:
         QUIT = False
         self.initial_grid()
         self.draw_grid()
+        # sleep(SPEED)
         while True:
             for event in pygame.event.get():
                 if event.type == QUIT: sys.exit()
